a58031fed00ffe8797e0d7c70582f7220e63cdff
ysafonov <487596@mail.muni.cz>
2019-04-21 13:10:09 +0200

    minor bug is fixed

--- RNG/src/cz/vutbr/feec/mkri/controllers/RandomResultControllers.java
+++ RNG/src/cz/vutbr/feec/mkri/controllers/RandomResultControllers.java
@@ -40,7 +40,7 @@ public class RandomResultControllers implements Initializable {
 	@FXML
 	private Rectangle rectangleResult;
 
-	private ArrayList<Integer> arrayForShowing;
+	private ArrayList<String> arrayForShowing;
 	private int shift;
 
 	/*
@@ -54,19 +54,19 @@ public class RandomResultControllers implements Initializable {
 	public void initialize(URL location, ResourceBundle resources) {
 		generateArrayListFirstGenerator();
 
-		randomNumberResult.setText(Integer.toString(arrayForShowing.get(0)));
+		randomNumberResult.setText(arrayForShowing.get(0));
 		if (arrayForShowing.size() >= 2)
-			randomNumberResult1.setText(Integer.toString(arrayForShowing.get(1)));
+			randomNumberResult1.setText(arrayForShowing.get(1));
 		if (arrayForShowing.size() >= 3)
-			randomNumberResult2.setText(Integer.toString(arrayForShowing.get(2)));
+			randomNumberResult2.setText(arrayForShowing.get(2));
 		if (arrayForShowing.size() >= 4)
-			randomNumberResult3.setText(Integer.toString(arrayForShowing.get(3)));
+			randomNumberResult3.setText(arrayForShowing.get(3));
 		if (arrayForShowing.size() >= 5)
-			randomNumberResult4.setText(Integer.toString(arrayForShowing.get(4)));
+			randomNumberResult4.setText(arrayForShowing.get(4));
 		if (arrayForShowing.size() >= 6)
-			randomNumberResult5.setText(Integer.toString(arrayForShowing.get(5)));
+			randomNumberResult5.setText(arrayForShowing.get(5));
 		if (arrayForShowing.size() >= 7)
-			randomNumberResult6.setText(Integer.toString(arrayForShowing.get(6)));
+			randomNumberResult6.setText(arrayForShowing.get(6));
 
 		rectangleResult.setOnScroll(new EventHandler() {
 			@Override
@@ -74,34 +74,34 @@ public class RandomResultControllers implements Initializable {
 				if (((ScrollEvent) event).getDeltaY() == -107) {
 					shift++;
 					if (shift < arrayForShowing.size()) {
-						randomNumberResult.setText(Integer.toString(arrayForShowing.get(shift)));
+						randomNumberResult.setText(arrayForShowing.get(shift));
 						if (shift < arrayForShowing.size() - 1) {
-							randomNumberResult1.setText(Integer.toString(arrayForShowing.get(shift+1)));
+							randomNumberResult1.setText(arrayForShowing.get(shift+1));
 						} else {
 							randomNumberResult1.setText("");
 						}
 						if (shift < arrayForShowing.size() - 2) {
-							randomNumberResult2.setText(Integer.toString(arrayForShowing.get(shift + 2)));
+							randomNumberResult2.setText(arrayForShowing.get(shift + 2));
 						} else {
 							randomNumberResult2.setText("");
 						}
 						if (shift < arrayForShowing.size() - 3) {
-							randomNumberResult3.setText(Integer.toString(arrayForShowing.get(shift + 3)));
+							randomNumberResult3.setText(arrayForShowing.get(shift + 3));
 						} else {
 							randomNumberResult3.setText("");
 						}
 						if (shift < arrayForShowing.size() - 4) {
-							randomNumberResult4.setText(Integer.toString(arrayForShowing.get(shift + 4)));
+							randomNumberResult4.setText(arrayForShowing.get(shift + 4));
 						} else {
 							randomNumberResult4.setText("");
 						}
 						if (shift < arrayForShowing.size() - 5) {
-							randomNumberResult5.setText(Integer.toString(arrayForShowing.get(shift + 5)));
+							randomNumberResult5.setText(arrayForShowing.get(shift + 5));
 						} else {
 							randomNumberResult5.setText("");
 						}
 						if (shift < arrayForShowing.size() - 6) {
-							randomNumberResult6.setText(Integer.toString(arrayForShowing.get(shift + 6)));
+							randomNumberResult6.setText(arrayForShowing.get(shift + 6));
 						} else {
 							randomNumberResult6.setText("");
 						}
@@ -112,12 +112,7 @@ public class RandomResultControllers implements Initializable {
 	}
 
 	private void generateArrayListFirstGenerator() {
-		arrayForShowing = new ArrayList<>();
-		//GeneratorMath.setArray(arrayForShowing, 100);
-		
-		for (int i = 0; i < Main.generator_configuration.OUTPUT.size(); i++) {
-			arrayForShowing.add(Integer.parseInt(Main.generator_configuration.OUTPUT.get(i)));
-		}
+		arrayForShowing = Main.generator_configuration.OUTPUT;
 	}
 
 }
